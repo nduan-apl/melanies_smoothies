ddc044a0d8ee3b248ff3bb330a6bb2a18f1f9f82
nduan-apl <nicholas.duan@albertaprecisionlabs.ca>
2025-04-02 16:58:38 -0600

    Update streamlit_app.py
    smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(query).collect()
         st.success('Your Smoothie is ordered', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
