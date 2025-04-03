d76aee1690f7e47371460facf1af97d08526220e
nduan-apl <nicholas.duan@albertaprecisionlabs.ca>
2025-04-03 10:02:50 -0600

    Update streamlit_app.py
    code line movement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
+import requests
 import streamlit as st
+
 from snowflake.snowpark.functions import col, when_matched
 
 helpful_links = [
@@ -36,6 +38,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -53,7 +57,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
