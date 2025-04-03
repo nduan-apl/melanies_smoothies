608740db760e35a67f8bd927b9308af7c7d59bb0
nduan-apl <nicholas.duan@albertaprecisionlabs.ca>
2025-04-03 10:30:18 -0600

    Update streamlit_app.py
    caps sensitive

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write(f'Search value for {fruit_name} is {search_on}')
         
         st.subheader(f"{fruit_chosen} Nutrition Information")
